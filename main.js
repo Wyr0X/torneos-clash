bf08965fa475c97bc1a1f15fa83b04f06672dcbc
Alexis Caraballo <alexiscaraballo96@gmail.com>
2021-02-12 18:53:27 -0300

    Seeded random, copyright & fixes

--- main.js
+++ main.js
@@ -10,6 +10,9 @@ let sortear_cartas;
 
 let loading_bar;
 let porcentaje_cargado = 0;
+let timer_ocultar_barra;
+
+let random_seed;
 
 window.addEventListener('load', () => {
     const primer_jugador = document.querySelector('.jugador');
@@ -40,6 +43,9 @@ window.addEventListener('load', () => {
 
     // Para eventos e indicationes específicas
     window.esDispositivoMovil = getDispositivoMovil();
+
+    // Para la seed
+    definirStringHash();
 });
 
 function modificaJugador(event) {
@@ -126,7 +132,7 @@ function crearTorneo(event) {
     // Info
     const info = document.createElement('p');
     if (esDispositivoMovil) {
-        info.textContent = 'Preisona sobre un jugador para marcarlo como ganador y avanzar a la siguiente ronda. Mantén presionado para revertir un enfrentamiento.';
+        info.textContent = 'Presiona sobre un jugador para marcarlo como ganador y avanzar a la siguiente ronda. Mantén presionado para revertir un enfrentamiento.';
     } else {
         info.textContent = 'Haz clic sobre un jugador para marcarlo como ganador y avanzar a la siguiente ronda. Clic derecho para revertir un enfrentamiento.';
     }
@@ -144,10 +150,13 @@ function crearTorneo(event) {
         jugador.setAttribute('disabled', true);
     });
 
+    // Seteamos la seed (si se usa)
+    setRandomSeed(document.querySelector('#semilla').value);
+
     // Ordenamos al azar
     const jugadores = [];
     for (let i = 0; i < cantidad_jugadores; i++) {
-        const indice = Math.floor(Math.random() * jugadores_sin_ordenar.length);
+        const indice = Math.floor(randomValue() * jugadores_sin_ordenar.length);
         jugadores.push(jugadores_sin_ordenar[indice]);
         jugadores_sin_ordenar.splice(indice, 1);
     }
@@ -373,20 +382,24 @@ function sortearCartas(event) {
         behavior: 'smooth'
     });
 
+    // Seteamos la seed a 0 para ver cartas aleatorias en la animación
+    setRandomSeed(0);
+
     // Esperamos 1 segundo
     setTimeout(barajarCartas, 1000);
 
-    let timer;
+    let termino_de_barajar = false;
 
     // Empezamos a barajar
     function barajarCartas() {
         // Cambiamos a una carta aleatoria cada X frames
-        timer = requestAnimationFrame(cambiarCartas);
-        // Barajamos durante 3 segundos
+        requestAnimationFrame(cambiarCartas);
+        // Barajamos durante un poco menos de 3 segundos
         setTimeout(() => {
-            cancelAnimationFrame(timer);
-            mostrarCartasFinales();
-        }, 3000);
+            termino_de_barajar = true;
+            // Seteamos la seed elegida (si se usa)
+            setRandomSeed(document.querySelector('#semilla').value);
+        }, 2850);
     }
 
     const elegidas = new Set();
@@ -394,7 +407,6 @@ function sortearCartas(event) {
 
     function cambiarCartas() {
         frame++;
-
         // Cambiamos de carta cada X frames
         if (frame >= 10) {
             // Por cada carta
@@ -402,7 +414,7 @@ function sortearCartas(event) {
                 // Elegimos una carta al azar
                 let indice;
                 do {
-                    indice = Math.floor(Math.random() * cartas.length);
+                    indice = Math.floor(randomValue() * cartas.length);
                 // Si ya está elegida, elegimos otra
                 } while(elegidas.has(indice));
                 // Liberamos la carta que usaba antes
@@ -413,11 +425,13 @@ function sortearCartas(event) {
                 // Marcamos como ya elegida
                 elegidas.add(indice);
             }
-
             frame = 0;
+            // La última barajada
+            if (termino_de_barajar) {
+                return mostrarCartasFinales();
+            }
         }
-
-        timer = requestAnimationFrame(cambiarCartas);
+        requestAnimationFrame(cambiarCartas);
     }
 
     function mostrarCartasFinales() {
@@ -437,7 +451,7 @@ function sortearCartas(event) {
         // Volvemos a habilitar el botón
         event.target.removeAttribute('disabled');
         // Esperamos un momento y removemos la barra de carga
-        setTimeout(ocultarBarraDeCarga, 500);
+        timer_ocultar_barra = setTimeout(ocultarBarraDeCarga, 500);
     }
 }
 
@@ -460,7 +474,7 @@ fetch('https://proxy.royaleapi.dev/v1/cards', {
     // Habilitamos el botón para sortear cartas
     sortear_cartas.removeAttribute('disabled');
     // Esperamos un momento y removemos la barra de carga
-    setTimeout(ocultarBarraDeCarga, 500);
+    timer_ocultar_barra = setTimeout(ocultarBarraDeCarga, 500);
 })
 .catch(() => {
     // Mostramos el error
@@ -505,6 +519,7 @@ function agregarPorcentajeDeCarga(porcentaje) {
 }
 
 function resetearBarraDeCarga() {
+    clearTimeout(timer_ocultar_barra);
     loading_bar.parentElement.classList.remove('fade-out');
     loading_bar.classList.remove('load-bar');
     porcentaje_cargado = 0;
@@ -525,4 +540,37 @@ function getDispositivoMovil() {
     let check = false;
     (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
     return check;
+}
+
+function setRandomSeed(seed) {
+    if (seed) {
+        random_seed = seed.hashCode();
+    } else {
+        random_seed = 0;
+    }
+}
+
+// Función random personalizada:
+// Si la seed es 0, funciona igual que Math.random. Si no, usa la seed para generar los números.
+function randomValue() {
+    if (random_seed) {
+        random_seed = random_seed * 16807 % 2147483647;
+        return (random_seed - 1) / 2147483646;
+    } else {
+        return Math.random();
+    }
+}
+
+function definirStringHash() {
+    Object.defineProperty(String.prototype, 'hashCode', {
+        value: function() {
+            var hash = 0, i, chr;
+            for (i = 0; i < this.length; i++) {
+                chr   = this.charCodeAt(i);
+                hash  = ((hash << 5) - hash) + chr;
+                hash |= 0; // Convert to 32bit integer
+            }
+            return hash;
+        }
+    });
 }
